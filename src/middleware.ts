18e7aa9f032c851eda7abeebacc9b68ac8ca3ad5
Luis fernando JU <luigfmp@gmail.com>
2024-11-30 23:55:58 -0500

    refactor: :wrench: add headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 const isPublicRoute = createRouteMatcher([
   '/',
@@ -16,6 +17,13 @@ export default clerkMiddleware((auth, request) => {
     auth().protect()
   }
 })
+
+export function middleware(request: NextRequest) {
+  const headers = new Headers(request.headers)
+  headers.set('x-current-path', request.nextUrl.pathname)
+  return NextResponse.next({ headers })
+}
+
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 }
