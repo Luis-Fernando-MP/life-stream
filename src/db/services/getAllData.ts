03cb874768e27306a68874bbc1000799a982f6b5
Luis fernando JU <luigfmp@gmail.com>
2024-12-02 13:35:51 -0500

    fix: :bug: fix url

--- src/db/services/getAllData.ts
+++ src/db/services/getAllData.ts
@@ -5,7 +5,7 @@ export async function getAllData(token?: string | null) {
   try {
     let headers = undefined
     if (token) headers = { Authorization: `Bearer ${token}` }
-    const response = await fetch(`${URL}/api/allDataData`, { headers })
+    const response = await fetch(`${URL}/api/api/allData`, { headers })
     if (!response.ok) throw new Error(`Error fetching all data: ${response.statusText}`)
     const all = (await response.json()) as ApiAllData
     return all
