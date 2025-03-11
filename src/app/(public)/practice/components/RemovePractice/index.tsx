d2a233bf8286098c8e793b0087332819c4613728
Juls <luigfmp@gmail.com>
2025-03-11 11:34:08 -0500

    refactor: :fire: Remove practice module components and related files

--- src/app/(public)/practice/components/RemovePractice/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { JSX } from 'react'
-
-const RemovePractice = (): JSX.Element => {
-  return <div className='practiceActions-view__container animate-blurred-fade-in'>remove</div>
-}
-
-export default RemovePractice
